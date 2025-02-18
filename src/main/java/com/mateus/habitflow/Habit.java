49f098f0f32924dd740f5102cf3940ab8f3b5f84
Mateus Queiroz <romaomatq@gmail.com>
2025-02-18 11:35:18 -0500

    Implemented basic functions of Habit class

--- src/main/java/com/mateus/habitflow/Habit.java
+++ src/main/java/com/mateus/habitflow/Habit.java
@@ -1,8 +1,49 @@
 package com.mateus.habitflow;
+import java.time.DayOfWeek;
+import java.time.LocalDate;
 
 public class Habit {
-    
-    public Habit() { 
-        
+    private final String category;
+    private final String description; 
+    private final int recurrence; //completions per week
+    private int completionsThisWeek; 
+    private int streak;
+    private LocalDate startOfWeek;
+
+    public Habit(String category, String description, int recurrence) { 
+        this.category = category;
+        this.description = description;
+        this.recurrence = recurrence;
+        this.streak = 0; 
+        this.completionsThisWeek = 0;
+        this.startOfWeek = setStartOfWeek();
     }
+
+    public final LocalDate setStartOfWeek() {
+        LocalDate today = LocalDate.now();
+        startOfWeek = today.with(DayOfWeek.MONDAY);
+        return startOfWeek;
+    }
+
+    public void checkStartOfWeek(){
+        LocalDate today = LocalDate.now();
+        if (today.isAfter(startOfWeek.plusDays(6))) {
+            startOfWeek = setStartOfWeek();
+            completionsThisWeek = 0;
+        }
+
+    }
+    
+    public void updateHabitProgress() {
+        checkStartOfWeek();
+        completionsThisWeek++;
+    }
+
+
+    public String getCategory() { return category;}
+    public String getDescription() { return description; }
+    public int recurrence() { return recurrence; }
+    public int getStreak() { return streak; }
+    public int getCompletions() { return completionsThisWeek; }
+
 }

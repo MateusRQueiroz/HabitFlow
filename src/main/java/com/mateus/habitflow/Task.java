27930863de2941225949c68a1b13912e362cb222
Mateus Queiroz <romaomatq@gmail.com>
2025-02-20 01:04:21 -0500

    Fix count id bug

--- src/main/java/com/mateus/habitflow/Task.java
+++ src/main/java/com/mateus/habitflow/Task.java
@@ -1,4 +1,5 @@
 package com.mateus.habitflow;
+
 import java.util.concurrent.atomic.AtomicInteger;
 
 public class Task {
@@ -10,33 +11,25 @@ public class Task {
     private final int id;
     private final int priority;
         
-        public Task(String category, String description, String due_date, int status, int priority) { 
-            this.category = category;
-            this.description = description;
-            this.due_date = due_date;
-            this.status = status;
-            this.priority = priority; 
-            this.id = count.incrementAndGet();
+    public Task(String category, String description, String due_date, int status, int priority) { 
+        this.category = category;
+        this.description = description;
+        this.due_date = due_date;
+        this.status = status;
+        this.priority = priority; 
+        this.id = count.incrementAndGet();
     }
 
-    public void changeStatus(int  new_status) {
+    public void changeStatus(int new_status) {
         status = new_status;
     }
-// overdue, In progress, pending, on hold, completed, cancelled
+
     public String getStatusString() {
         switch (status) {
-            case 1 -> {
-                return "overdue";
-            }
-            case 2 -> {
-                return "in progress";
-            }
-            case 3 -> {
-                return "pending";
-            }
-            case 4 -> {
-                return "on hold";
-            }
+            case 1 -> { return "overdue"; }
+            case 2 -> { return "in progress"; }
+            case 3 -> { return "pending"; }
+            case 4 -> { return "on hold"; }
             default -> {
                 System.out.println("Not an option");
                 return null;
@@ -44,21 +37,12 @@ public class Task {
         }
     }
 
-// High, medium, low, optional
     public String getPriorityString() {
         switch (priority) {
-            case 1 -> {
-                return "High";
-            }
-            case 2 -> {
-                return "Medium";
-            }
-            case 3 -> {
-                return "Low";
-            }
-            case 4 -> {
-                return "Optional";
-            }
+            case 1 -> { return "High"; }
+            case 2 -> { return "Medium"; }
+            case 3 -> { return "Low"; }
+            case 4 -> { return "Optional"; }
             default -> {
                 System.out.println("Not an option");
                 return null;
@@ -66,10 +50,14 @@ public class Task {
         }
     }
 
-    public String getCategory() { return category;}
-    public String getDescription() {return description;}
-    public String getDue_date() {return due_date;}
-    public int getPriority() {return priority;}
-    public int getStatus() {return status;}
+    public String getCategory() { return category; }
+    public String getDescription() { return description; }
+    public String getDue_date() { return due_date; }
+    public int getPriority() { return priority; }
+    public int getStatus() { return status; }
     public int getId() { return id; }
+
+    public static void updateCounter(int maxId) {
+        count.set(maxId);
+    }
 }

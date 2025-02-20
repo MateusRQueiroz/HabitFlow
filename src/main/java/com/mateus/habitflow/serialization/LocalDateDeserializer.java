44be681605b0ae4d91cade794cad408860176b94
Mateus Queiroz <romaomatq@gmail.com>
2025-02-20 01:09:14 -0500

    Fix implementation of serialization to work with json

--- src/main/java/com/mateus/habitflow/serialization/LocalDateDeserializer.java
+++ src/main/java/com/mateus/habitflow/serialization/LocalDateDeserializer.java
@@ -2,6 +2,7 @@ package com.mateus.habitflow.serialization;
 
 import java.lang.reflect.Type;
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 import com.google.gson.JsonDeserializationContext;
 import com.google.gson.JsonDeserializer;
@@ -9,16 +10,10 @@ import com.google.gson.JsonElement;
 import com.google.gson.JsonParseException;
 
 public class LocalDateDeserializer implements JsonDeserializer<LocalDate> {
+    private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd");
 
     @Override
     public LocalDate deserialize(JsonElement json, Type typeOfT, JsonDeserializationContext context) throws JsonParseException {
-        if (json != null && json.isJsonObject()) {
-            JsonElement dateElement = json.getAsJsonObject().get("date");  // Get the "date" property
-
-            if (dateElement != null) {
-                return LocalDate.parse(dateElement.getAsString());  // Parse the string to LocalDate
-            }
-        }
-        throw new JsonParseException("Invalid or missing 'date' property in JSON.");
+        return LocalDate.parse(json.getAsString(), FORMATTER);
     }
 }
--- src/main/java/com/mateus/habitflow/serialization/LocalDateSerializer.java
+++ src/main/java/com/mateus/habitflow/serialization/LocalDateSerializer.java
@@ -1,18 +1,19 @@
 package com.mateus.habitflow.serialization;
 
-import java.lang.reflect.Type;
-import java.time.LocalDate;
-
 import com.google.gson.JsonElement;
-import com.google.gson.JsonObject;
+import com.google.gson.JsonPrimitive;
 import com.google.gson.JsonSerializationContext;
 import com.google.gson.JsonSerializer;
 
+import java.lang.reflect.Type;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 public class LocalDateSerializer implements JsonSerializer<LocalDate> {
+    private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+
     @Override
     public JsonElement serialize(LocalDate src, Type typeOfSrc, JsonSerializationContext context) {
-        JsonObject jsonObject = new JsonObject();  
-        jsonObject.addProperty("date", src.toString());  
-        return jsonObject;  
+        return new JsonPrimitive(src.format(FORMATTER));
     }
 }
